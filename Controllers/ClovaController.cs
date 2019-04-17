e73176f0ce6585c64a30a50fc2d5073942587f38
chomado (Madoka Chiyoda) <madoka.madoka@microsoft.com>
2019-04-17 13:34:58 +0900

    Initial commit

---
+++ Controllers/ClovaController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CEK.CSharp.Models;
+using CEK.CSharp;
+
+namespace EchoBot.Controllers
+{
+    [Route("api/clova")]
+    public class ClovaController : Controller
+    {
+        private ClovaClient client;
+
+        public ClovaController()
+        {
+            client = new ClovaClient();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            CEKRequest request = await client.GetRequest(
+                Request.Headers["SignatureCEK"],
+                Request.Body
+            );
+
+            var response = new CEKResponse();
+
+            switch (request.Request.Type)
+            {
+                case RequestType.LaunchRequest:
+                    {
+                        response.AddText("こんにちは！これ食べれるニャンボットです。人間の食べ物を入力すると、これは猫が食べて大丈夫かどうかを答えるよ");
+                        response.ShouldEndSession = false;
+                        break;
+                    }
+                case RequestType.SessionEndedRequest:
+                    {
+                        response.AddText("またね～");
+                        break;
+                    }
+                case RequestType.IntentRequest:
+                    {
+                        switch (request.Request.Intent.Name)
+                        {
+                            case "CatIntent": // "パン"
+                                {
+                                    // 食べれるものが渡ってくるので何か処理をする TODO!
+                                    response.AddText(text: $"{request.Request.Intent.Slots.FirstOrDefault().Value.Value}の話ですか？");
+                                    response.ShouldEndSession = false;
+                                    break;
+                                }
+                            case "ChomadoIntent": // "ちょまど"
+                                {
+                                    response.AddText("ちょまどさん、さすがオトナです！素敵！すばらしい！松屋！");
+                                    response.ShouldEndSession = false;
+                                    break;
+                                }
+                            case "Clova.GuideIntent":
+                                {
+                                    response.AddText("パン、と言ってみてください。");
+                                    response.ShouldEndSession = false;
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+            }
+            return new OkObjectResult(response);
+        }
+
+    }
+}
---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=190420linebot-Web-Deploy.pubxml -p:Password=mZq41GfutDa8Aq5EKYrFL8t1YtlF4rg5W1MkQ35519mHYxHiu2ya6TGrpuHE
+
