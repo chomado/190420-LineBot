891628c4b4b2dc43c4284d61f740dd5da853f0e9
chomado (Madoka Chiyoda) <madoka.madoka@microsoft.com>
2019-04-18 23:39:03 +0900

    LINE push

--- Controllers/ClovaController.cs
+++ Controllers/ClovaController.cs
@@ -7,6 +7,7 @@ using CEK.CSharp.Models;
 using CEK.CSharp;
 using Line.Messaging;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace EchoBot.Controllers
 {
@@ -15,15 +16,13 @@ namespace EchoBot.Controllers
     {
         private ClovaClient _client;
         private IConfiguration _configuration;
+        private readonly ILogger _logger;
 
-        public ClovaController()
+        public ClovaController(IConfiguration configuration, ILoggerFactory loggerFactory)
         {
             _client = new ClovaClient();
-        }
-
-        public ClovaController(IConfiguration configuration)
-        {
             _configuration = configuration;
+            _logger = loggerFactory.CreateLogger<ClovaController>();
         }
 
         [HttpPost]
@@ -36,62 +35,84 @@ namespace EchoBot.Controllers
 
             var response = new CEKResponse();
 
-            switch (request.Request.Type)
+            try
             {
-                case RequestType.LaunchRequest:
-                    {
-                        response.AddText("こんにちは！これ食べれるニャンボットです。人間の食べ物を入力すると、これは猫が食べて大丈夫かどうかを答えるよ");
-                        response.ShouldEndSession = false;
-                        break;
-                    }
-                case RequestType.SessionEndedRequest:
-                    {
-                        response.AddText("またね～");
-                        break;
-                    }
-                case RequestType.IntentRequest:
-                    {
-                        switch (request.Request.Intent.Name)
+                switch (request.Request.Type)
+                {
+                    case RequestType.LaunchRequest:
                         {
-                            case "CatIntent": // "パン"
-                                {
-                                    var food = request.Request.Intent.Slots.FirstOrDefault().Value.Value;
-                                    // 食べれるものが渡ってくるので何か処理をする TODO!
-                                    response.AddText(text: $"{food}の話ですか？");
-
-                                    // LINE messaging に push
-                                    string accessToken = _configuration.GetValue<string>("LINEMessaginAPIKey");
-
-                                    var messagingClient = new LineMessagingClient(channelAccessToken: accessToken);
-                                    await messagingClient.PushMessageAsync(
-                                        to: request.Session.User.UserId,
-                                        messages: new List<ISendMessage>
-                                        {
-                                            new TextMessage($"{food}"),
-                                        }
-                                    );
-                                    response.ShouldEndSession = false;
-                                    break;
-                                }
-
-                            case "ChomadoIntent": // "ちょまど"
-                                {
-                                    response.AddText("ちょまどさん、さすがオトナです！素敵！すばらしい！松屋！");
-                                    response.ShouldEndSession = false;
-                                    break;
-                                }
-                            case "Clova.GuideIntent":
-                                {
-                                    response.AddText("パン、と言ってみてください。");
-                                    response.ShouldEndSession = false;
-                                    break;
-                                }
+                            response.AddText("こんにちは！これ食べれるニャンボットです。人間の食べ物を入力すると、これは猫が食べて大丈夫かどうかを答えるよ");
+                            response.ShouldEndSession = false;
+                            break;
                         }
-                        break;
-                    }
+                    case RequestType.SessionEndedRequest:
+                        {
+                            response.AddText("またね～");
+                            break;
+                        }
+                    case RequestType.IntentRequest:
+                        {
+                            switch (request.Request.Intent.Name)
+                            {
+                                case "HelloIntent":
+                                    {
+                                        response.AddText("こんにちは！これ食べれるニャンボットだよ！");
+                                        response.ShouldEndSession = false;
+                                        break;
+                                    }
+                                case "ChomadoIntent": // "ちょまど"
+                                    {
+                                        response.AddText("ちょまどさん、さすがオトナです！素敵！すばらしい！松屋！");
+                                        response.ShouldEndSession = false;
+                                        break;
+                                    }
+                                case "Clova.GuideIntent":
+                                    {
+                                        response.AddText("パン、と言ってみてください。");
+                                        response.ShouldEndSession = false;
+                                        break;
+                                    }
+                                case "CatIntent": // "パン"
+                                    {
+                                        var food = request.Request.Intent.Slots.FirstOrDefault().Value.Value;
+                                        // 食べれるものが渡ってくるので何か処理をする TODO!
+                                        response.AddText(text: $"{food}の話ですか？");
+                                        // 「食べられるよ」/「食べられないよ」
+
+                                        // TODO: QnA で分からない時の処理を書く
+                                        
+
+                                        // LINE messaging に push
+                                        string accessToken = _configuration.GetValue<string>("LINEMessaginAPIKey");
+                                        string myUserId = _configuration.GetValue<string>("myUserID");
+
+                                        _logger.LogInformation($"{accessToken}: {myUserId}");
+
+                                        var messagingClient = new LineMessagingClient(channelAccessToken: accessToken);
+                                        await messagingClient.PushMessageAsync(
+                                            // to: request.Session.User.UserId,
+                                            to: myUserId,
+                                            messages: new List<ISendMessage>
+                                            {
+                                                new TextMessage($"{food}"),
+                                                new TextMessage($"解説はこれだよ"),
+                                            }
+                                        );
+
+                                        response.ShouldEndSession = false;
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
+                }
             }
+            catch (Exception ex)
+            {
+                response.AddText(ex.ToString());
+            }
+
             return new OkObjectResult(response);
         }
-
     }
 }
